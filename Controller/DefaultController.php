3f8427a948f47af7f59b6712c898652b844db6a5
Tom Davidson <tom@yuki-onna.co.uk>
2014-02-21 22:06:57 +0000

    Initial working foundation

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -15,7 +15,11 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+		$em = $this->getDoctrine()->getEntityManager();
+		$query = $em->createQuery('SELECT m FROM TomDavidsonMembershipBundle:Member m WHERE m.active = :active ORDER BY m.name')->setParameter('active', '1');
+		$members = $query->getResult();
         return array(
+			'members' => $members
 		);
     }
 }
