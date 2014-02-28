c2678881ee20881694842ecdbb3e87995792a1f8
Tom Davidson <tom@yuki-onna.co.uk>
2014-02-28 15:33:42 +0000

    Initial (rough) working module

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -10,16 +10,125 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="members_home")
      * @Template()
      */
-    public function indexAction()
-    {
-		$em = $this->getDoctrine()->getEntityManager();
-		$query = $em->createQuery('SELECT m FROM TomDavidsonMembershipBundle:Member m WHERE m.active = :active ORDER BY m.name')->setParameter('active', '1');
+    public function indexAction(Request $request) {
+		// Get a list of all members
+		$em = $this->getDoctrine()->getManager();
+		$query = $em->createQuery('SELECT m FROM TomDavidsonMembershipBundle:Member m WHERE m.status = :status ORDER BY m.name')->setParameter('status', 'A');
 		$members = $query->getResult();
+		// Set up the 'add' form
+		$newMember = new \TomDavidson\MembershipBundle\Entity\Member();
+		$form = $this->createFormBuilder($newMember)
+			->add('name',			'text',		array('label'=>'Your Name', 'required'=>true, 'max_length'=>100))
+			->add('organisation',	'text',		array('label'=>'Your Organisation', 'required'=>true, 'max_length'=>254))
+			->add('email',			'email',	array('label'=>'Your Email', 'required'=>true, 'max_length'=>100))
+			->add('sponsor1',		'entity',	array(
+				'class'			=> 'TomDavidsonMembershipBundle:Member',
+				'query_builder'	=> function($repository) { return $repository->createQueryBuilder('p')->where('p.status=:status')->setParameter('status', 'A')->orderBy('p.name', 'ASC'); },
+				'property'		=> 'name',
+				'label'			=> 'Sponsor'
+			))
+			->add('sponsor2',		'entity',	array(
+				'class'			=> 'TomDavidsonMembershipBundle:Member',
+				'query_builder'	=> function($repository) { return $repository->createQueryBuilder('p')->where('p.status=:status')->setParameter('status', 'A')->orderBy('p.name', 'ASC'); },
+				'property'		=> 'name',
+				'label'			=> 'Sponsor'
+			))
+			->getForm();
+		// Form handling
+		if ($request->getMethod() == 'POST') {
+			//$form->bindRequest($request);
+			$form->handleRequest($request);
+			$newMember->setJoined();
+			if ($form->isValid()) {
+				// Save to DB
+				$em->persist($newMember);
+				$em->flush();
+				// Redirect
+				return $this->redirect($this->generateUrl('members_home'));
+			}
+		}
         return array(
-			'members' => $members
+			'members'	=> $members,
+			'form'		=> $form->createView()
 		);
     }
+    /**
+     * @Route("/admin", name="members_admin")
+     * @Template()
+     */
+    public function adminAction(Request $request) {
+		// Get a list of all members
+		$em = $this->getDoctrine()->getManager();
+		$query = $em->createQuery('SELECT m FROM TomDavidsonMembershipBundle:Member m ORDER BY m.name');
+		$members = $query->getResult();
+//$vd = $members[0]->getSponsor1();
+//var_dump($vd);
+        return array(
+			'members'	=> $members
+		);
+	}
+    /**
+     * @Route("/admin/authorise/{id}", name="members_admin_authorise")
+     */
+    public function adminAuthoriseAction(Request $request, $id) {
+		$em = $this->getDoctrine()->getEntityManager();
+		$member = $em->getRepository('TomDavidsonMembershipBundle:Member')->find($id);
+		if ($member) {
+			$member->setStatus('A');
+			$em->flush();
+			return $this->redirect($this->generateUrl('members_admin'));
+		}else{
+			throw new \Exception('Member not found');
+		}
+	}
+    /**
+     * @Route("/admin/deactivate/{id}", name="members_admin_deactivate")
+     */
+    public function adminDeactivateAction(Request $request, $id) {
+		$em = $this->getDoctrine()->getEntityManager();
+		$member = $em->getRepository('TomDavidsonMembershipBundle:Member')->find($id);
+		if ($member) {
+			$member->setStatus('I');
+			$em->flush();
+			return $this->redirect($this->generateUrl('members_admin'));
+		}else{
+			throw new \Exception('Member not found');
+		}
+	}
+    /**
+     * @Route("/admin/delete/{id}", name="members_admin_delete")
+     */
+    public function adminDeleteAction(Request $request, $id) {
+		$em = $this->getDoctrine()->getEntityManager();
+		$member = $em->getRepository('TomDavidsonMembershipBundle:Member')->find($id);
+		if ($member) {
+			// Null out any other member's sponsor1_id for this deleted record to avoid orphans.
+			$qb = $em->createQueryBuilder();
+			$q = $qb->update('TomDavidsonMembershipBundle:Member', 'm')
+				->set('m.sponsor1', ':newSponsor')
+				->where('m.sponsor1 = :deletingMember')
+				->setParameter('newSponsor', null)
+				->setParameter('deletingMember', $member)
+				->getQuery();
+			$p = $q->execute();
+			// Null out any other member's sponsor2_id for this deleted record to avoid orphans.
+			$qb = $em->createQueryBuilder();
+			$q = $qb->update('TomDavidsonMembershipBundle:Member', 'm')
+				->set('m.sponsor2', ':newSponsor')
+				->where('m.sponsor2 = :deletingMember')
+				->setParameter('newSponsor', null)
+				->setParameter('deletingMember', $member)
+				->getQuery();
+			$p = $q->execute();
+			// Remove the member
+			$em->remove($member);
+			$em->flush();
+			return $this->redirect($this->generateUrl('members_admin'));
+		}else{
+			throw new \Exception('Member not found');
+		}
+	}
 }
--- Entity/Member.php
+++ Entity/Member.php
@@ -19,103 +19,51 @@ class Member
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\OneToOne(targetEntity="Member", inversedBy="sponsor1")
+	 * @ORM\OneToOne(targetEntity="Member", inversedBy="sponsor2")
      */
     private $id;
+	public function getId(){ return $this->id; }
 
     /**
      * @var string $name
      *
-     * @ORM\Column(name="name", type="string", length=64)
+     * @Assert\NotBlank()
+     * @ORM\Column(name="name", type="string", length=100)
      */
     private $name;
+	public function getName(){ return $this->name; }
+    public function setName($name) { $this->name = $name; }
 
     /**
      * @var string $organisation
      *
-     * @ORM\Column(name="organisation", type="string", length=64)
+     * @ORM\Column(name="organisation", type="string", length=254)
      */
     private $organisation;
+	public function getOrganisation(){ return $this->organisation; }
+    public function setOrganisation($organisation) { $this->organisation = $organisation; }
 
     /**
      * @var string $email
      *
-     * @ORM\Column(name="email", type="string", length=64)
+     * @Assert\NotBlank()
+     * @ORM\Column(name="email", type="string", length=254)
      */
     private $email;
+	public function getEmail(){ return $this->email; }
+    public function setEmail($email) { $this->email = $email; }
 
     /**
-     * @var string $active
+     * @var string $password
      *
-     * @ORM\Column(name="active", type="integer")
+     * @ORM\Column(name="password", type="string", length=254)
      */
-    private $active = 1;
-
-    /**
-     * @var datetime $date
-     *
-     * @ORM\Column(name="date", type="string")
-     */
-    private $date;
-
-    /**
-     * @var string $ip
-     *
-     * @ORM\Column(name="ip", type="string", length=64)
-     */
-    private $ip;
-
-    /**
-     * Get name
-     *
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    /**
-     * Get email
-     *
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-    /**
-     * Set email
-     *
-     * @param string $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * Set active
-     *
-     * @param boolean $active
-     */
-    public function setActive($active)
-    {
-		switch($active){
-			case true;
-				$this->active = 1;
-			break;
-			case false;
-				$this->active = 0;
-			break;
-			default;
-				throw new \Exception('Subscriber::active must be either true or false.');
-			break;
-		}
-    }
-
-    /**
-     * Is active
-     */
-    public function isActive()
-    {
-		if($this->active == 1){
+    private $password;
+	//public function getPassword(){ return $this->password; }
+    public function setPassword($password) { $this->password = crypt($password, 'p3PP3r'); }
+	public function checkPassword($password){
+		if($this->password == crypt($password, 'p3PP3r')){
 			return true;
 		}else{
 			return false;
@@ -123,31 +71,99 @@ class Member
 	}
 
     /**
-     * Set date
+     * @var string $status
      *
-     * @param string $date
+     * @ORM\Column(name="status", type="string", length=1)
      */
-    public function setDate($date = null)
-    {
-		if($date == null){
-			$this->date = date('Y-m-d');
+    private $status = 'U';
+	public function getStatus(){ return $this->status; }
+    public function setStatus($status) {
+		// A = active, I = inactive, U = unverified
+		if(in_array($status, array('A','I','U'))){
+			$this->status = $status;
 		}else{
-			if (($timestamp = strtotime($str)) === false) {
-				throw new \Exception('Date-like expected for Subscriber::setDate, given "'.$date.'"');
-			}else{
-				$this->date = date('Y-m-d', $timestamp);
-			}
+			throw new \Exception('Member::status must be one of: A/I/U.');
 		}
     }
 
     /**
-     * Set ip
+     * @var object $sponsor1
      *
-     * @param string $ip
+     * @ORM\OneToOne(targetEntity="Member")
+	 * @ORM\JoinColumn(name="sponsor1_id", referencedColumnName="id", nullable=false)
      */
-    public function setIP($ip)
-    {
-        $this->ip = $ip;
+    private $sponsor1;
+	public function getSponsor1(){ return $this->sponsor1; }
+    public function setSponsor1($sponsor1) { $this->sponsor1 = $sponsor1; }
+
+    /**
+     * @var object $sponsor2
+     *
+     * @ORM\OneToOne(targetEntity="Member")
+	 * @ORM\JoinColumn(name="sponsor2_id", referencedColumnName="id", nullable=false)
+     */
+    private $sponsor2;
+	public function getSponsor2(){ return $this->sponsor2; }
+    public function setSponsor2($sponsor2) { $this->sponsor2 = $sponsor2; }
+
+    /**
+     * @var datetime $joined
+     *
+	 * @Assert\Type("\DateTime")
+     * @ORM\Column(name="joined", type="datetime")
+     */
+    private $joined;
+	public function getJoined(){ return $this->joined; }
+    public function setJoined($joined = null) {
+		if($joined == null){
+			//$this->joined = date('Y-m-d H:i:s');
+			$this->joined = new \Datetime();
+		}else{
+			if (($timestamp = strtotime($joined)) === false) {
+				throw new \Exception('Date-like string expected for Member::setDate, given "'.$joined.'"');
+			}else{
+				//$this->date = date('Y-m-d', $timestamp);
+				$this->date = new \Datetime($timestamp);
+			}
+		}
     }
 
+	public function getSponsors(){
+		$sponsor1 = \Doctrine\Common\Util\Debug::export($this->sponsor1, 1);
+		if($sponsor1 === null){
+			$sponsor1 = '[DELETED MEMBER]';
+		}else{
+			$sponsor1 = $sponsor1->name;
+		}
+		$sponsor2 = \Doctrine\Common\Util\Debug::export($this->sponsor2, 1);
+		if($sponsor2 === null){
+			$sponsor2 = '[DELETED MEMBER]';
+		}else{
+			$sponsor2 = $sponsor2->name;
+		}
+		return $sponsor1.','.$sponsor2;
+	}
 }
+
+/*
+
+-- MySQL table creation
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE IF NOT EXISTS `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `organisation` varchar(254) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(254) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `password` varchar(254) COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `status` varchar(1) NOT NULL,
+  `sponsor1_id` int(11) NULL DEFAULT NULL,
+  `sponsor2_id` int(11) NULL DEFAULT NULL,
+  `joined` DATETIME COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+ALTER TABLE `website_transportmodeller_com`.`members` ADD INDEX ( `status` );
+INSERT INTO `website_transportmodeller_com`.`members` (`id`, `name`, `organisation`, `email`, `password`, `status`, `sponsor1_id`, `sponsor2_id`, `joined`) VALUES ('1', 'Tom Davidson', 'Test Organisation', 'tom@testdomain.com', NULL, 'A', 1, 1, NOW());
+
+ */
